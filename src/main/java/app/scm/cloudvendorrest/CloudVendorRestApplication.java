82a53893879ef283f7866eb24a50a50bf40a78db
motinxyz <motinsghs@gmail.com>
2024-12-04 15:30:30 +0600

    init commit

---
+++ src/main/java/app/scm/cloudvendorrest/CloudVendorRestApplication.java
@@ -0,0 +1,13 @@
+package app.scm.cloudvendorrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudVendorRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudVendorRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/scm/cloudvendorrest/CloudVendorRestApplicationTests.java
@@ -0,0 +1,13 @@
+package app.scm.cloudvendorrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudVendorRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
